535450671d5c708ad51323428ad4977711127d17
Mairon Schneider Cardoso <44898984+MaironSC@users.noreply.github.com>
2021-11-27 23:14:49 -0300

    Delete Thematic systems and signal directory

--- Thematic systems and signal/Continuous Time Systems/Questao1g.m
+++
@@ -1,8 +0,0 @@
-t=0:0.001:10;
-y_imp = -0.5668*exp(-0.3542*t) + 0.5668*exp(-5.6457*t);
-subplot(1,1,1)
-plot(t,y_imp,'Linewidth',1.5');
-
-lgd = legend('Resposta ao impulso');
-title(lgd,'Questão 1g)')
-grid on;
