9a8a076be6ba6d02e8c4d33ce2ed597cc8901f07
Mairon Schneider Cardoso <44898984+MaironSC@users.noreply.github.com>
2021-11-27 23:14:30 -0300

    Delete Thematic systems and signal/Operations with signals, elementary signals and properties of systems directory

--- Thematic systems and signal/Operations with signals, elementary signals and properties of systems/Exercicio2.m
+++
@@ -1,6 +0,0 @@
-t=-5:0.001:5;
-y = (3*t-2).*(u(-t+3)-(u(-t+4)+u(-t+1))).*u(t+2);
-plot(t,y)
-title('y(t) = (3t-2) * (u(-t+3) - (u(-t+4) + u(-t+1))) * u(t+2)')
-xlabel('Tempo')  
-grid on;
--- Thematic systems and signal/Operations with signals, elementary signals and properties of systems/jexp.m
+++
@@ -1,5 +0,0 @@
-function [y] = jexp(delta, w, t)
-%s = delta +j*w
-y = exp((delta+(1i*w))*t);
-end
-
